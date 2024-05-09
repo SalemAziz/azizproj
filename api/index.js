5fa1f8b615ff0185fddbddfad84e1a5cfc9e0ab1
SalemAziz <163313738+SalemAziz@users.noreply.github.com>
2024-05-09 13:47:36 +0100

    Create sign up API route / npm i bcrypt js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.js';
+import authRoutes from './routes/auth.js';
+
 
 dotenv.config();
 
@@ -15,9 +17,13 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
 app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes)
+
